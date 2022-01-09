3f48b667047b6ac2d3566b42ea97c95a5236fc6b
Yulia Lime <lime666lime@gmail.com>
2022-01-10 00:46:10 +0200

    styles added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   validates :title, :content, presence: true
   validates :title, length: { in: 30..80 }
-  validates :content, length: { in: 250..2500 }
+  validates :content, length: { in: 250..5000 }
   belongs_to :author
   has_rich_text :content
 end
