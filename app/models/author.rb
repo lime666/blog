25028c89af4f76d15d9e1641311ba7851b6f46b8
Yulia Lime <lime666lime@gmail.com>
2022-01-13 18:30:55 +0200

    name deleted

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ApplicationRecord
+  #dependent :destroy
   validates :first_name, :last_name, presence: true
   has_many :posts
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_104309) do
+ActiveRecord::Schema.define(version: 2022_01_13_162751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 2021_12_31_104309) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "name"
     t.string "title"
     t.text "content"
     t.string "image"
